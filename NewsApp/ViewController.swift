6e7a97ce54b044038312a5ce6c5c68a89300a3c9
Nitesh <niteshmalhotra00@gmail.com>
2025-07-03 09:26:48 -0400

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Nitesh Malhotra on 2025-07-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
